099e83c44254efca61360527bc7b9b9b19de5ee9
Mohammad <mohammad93p@yahoo.com>
2022-01-26 15:53:51 +0330

    change ui bottomNav

--- lib/config/route/home_screen.dart
+++ lib/config/route/home_screen.dart
@@ -1,97 +1,73 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sample/constant/string_routes.dart';
 import 'package:sample/constant/strings.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectIndex= 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text("Custom BottomNavigationBar"),
+        ),
         body: Center(
           child: Text(Strings().welcome),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.pushNamed(context, customScrollViewScreen);
-          },
-          child: Text("Scroll"),
-        ),
-        bottomNavigationBar: _bottomNavBar(context));
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {
+        //     Navigator.pushNamed(context, customScrollViewScreen);
+        //   },
+        //   child: Text("Scroll"),
+        // ),
+        bottomNavigationBar: _bottomNavBar());
   }
-}
+  Widget _bottomNavBar() {
+    return Container(
+      height: 53,
+      decoration: BoxDecoration(
+          color: Colors.white, boxShadow: [BoxShadow(color: Colors.grey)]),
+      child: Row(
+        children: [
+          _itemBottomNav(Icons.home,0),
+          _itemBottomNav(Icons.favorite,1),
+          _itemBottomNav(Icons.search,2),
+          _itemBottomNav(Icons.person,3),
+        ],
+      ),
+    );
+  }
 
-Widget _bottomNavBar(BuildContext context) {
-  return Stack(
-    children: [
-      Positioned(
-        bottom: 0,
-        child: ClipPath(
-          clipper: NavBarClipper(),
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: 60,
-            color: Colors.blue,
+  Widget _itemBottomNav(IconData icon,int index) {
+    return Expanded(
+      child: InkWell(
+        onTap: (){
+          setState(() {
+            _selectIndex = index;
+          });
+        },
+        child: Container(
+          height: 45,
+          decoration:_selectIndex == index ? BoxDecoration(
+            color: Colors.white,//Color.fromARGB(255, 220, 220, 220),
+            shape: BoxShape.circle,
+            boxShadow: [BoxShadow(color: Colors.grey,blurRadius: 1,spreadRadius: .1)]
+          ): BoxDecoration(),
+          child: Icon(
+            icon,
+            color:_selectIndex == index ? Colors.blueGrey.shade600: Colors.grey,
+            size: _selectIndex == index ? 25 : 18,
           ),
         ),
       ),
-      Positioned(
-        bottom: 45,
-        width: MediaQuery.of(context).size.width,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            _buildNavItem(),
-            SizedBox(width: 0,),
-            _buildNavItem(),
-            SizedBox(width: 0,),
-            _buildNavItem(),
-  ]
-        ),
-      )
-    ],
-  );
-}
-
-_buildNavItem(){
-  return CircleAvatar(
-    radius: 30,
-    backgroundColor: Colors.blue.shade800,
-    child: CircleAvatar(
-      radius: 28,
-      backgroundColor: Colors.white70,
-    ),
-  );
-}
-
-class NavBarClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    Path path = Path();
-    var sw = size.width;
-    var sh = size.height;
-
-    path.cubicTo(sw / 12, 0, sw / 12, 2 * sh / 5, 2 * sw / 12, 2 * sh / 5);
-    path.cubicTo(3 * sw / 12, 2 * sh / 5, 3 * sw / 12, 0, 4 * sw / 12, 0);
-
-    path.cubicTo(5 * sw / 12, 0, 5 * sw / 12, 2 * sh / 5, 6 * sw / 12, 2 * sh / 5);
-    path.cubicTo(7 * sw / 12, 2 * sh / 5, 7 * sw / 12, 0, 8 * sw / 12, 0);
-
-    path.cubicTo(9 * sw / 12, 0, 9 * sw / 12, 2 * sh / 5, 10 * sw / 12, 2 * sh / 5);
-    path.cubicTo(11 * sw / 12, 2 * sh / 5, 11 * sw / 12, 0, 12 * sw / 12, 0);
-
-    path.cubicTo(13 * sw / 12, 0, 13 * sw / 12, 2* sh /5, 14 * sw / 12, 2* sh / 5);
-    // path.cubicTo(15 * sw / 12, 2 * sh / 5, 12 * sw / 12, 0,16* sw/15, 0);
-    //
-    // path.cubicTo(17 * sw / 12, 0, 17 * sw / 12, 2* sh /5, 18 * sw / 12, 2* sh / 5);
-    path.cubicTo(15 * sw / 12, 2 * sh / 5, 12 * sw / 12, 0,sw, 0);
-    path.lineTo(sw, sh);
-    path.lineTo(0, sh);
-    path.close();
-    return path;
+    );
   }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
+
+
